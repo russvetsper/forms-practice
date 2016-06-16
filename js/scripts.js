14acafd86f134a410b429b07de83e374f66477f2
Russ Vetsper and Steven Tran <russ+steven@>
2016-06-15 17:28:17 -0700

    add new form

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var person1Input =$("input#person1").val();
+    var person2Input =$("input#person2").val();
+    var person3Input =$("input#person3").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".person3").text(person3Input);
+
+    event.preventDefault();
+  });
+});
